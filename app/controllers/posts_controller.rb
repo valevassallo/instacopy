f0b54a1cab213eb2c9c74ca6cf06ea7cc53db1e0
Cesar Cachay <cesarcachay1996@gmail.com>
2019-04-24 11:24:39 -0500

    Add decorator and refactor queries

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.includes([:user, :media, comments: [:user], likes: [:user]]).all
   end
+
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def fullname(user)
+    # self.first_name + " " + self.last_name 
+    "#{user.first_name} #{user.last_name}"
+  end
+
 end
